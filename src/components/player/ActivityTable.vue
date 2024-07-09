7dc4499f37f6703c6b0e7284fa70c7c84f627059
Pei Ying <eee317@gmail.com>
2024-07-09 21:03:13 +0800

    玩家評論彈窗

--- src/components/player/ActivityTable.vue
+++ src/components/player/ActivityTable.vue
@@ -5,6 +5,12 @@ import { useRouter } from 'vue-router';
 import toLocalString from '@/utilities/toLocalString';
 import EmptyTable from '@/components/common/EmptyTable.vue';
 import Pagination from '@/components/common/Pagination.vue';
+import starRatingModal from '@/components/player/starRatingModal.vue';
+
+const RatingModal = ref(null);
+const openRatingModal = () => {
+    RatingModal.value.myModalShow();
+};
 
 const PaymentStatus = {
     pending: '尚未付款',
@@ -28,6 +34,7 @@ const totalPages = computed(() => {
 const paginatedList = computed(() => {
     const start = (currentPage.value - 1) * itemsPerPage;
     const end = start + itemsPerPage;
+    console.log('props.activityList', props.activityList);
     return props.activityList.slice(start, end);
 });
 const setStatus = (x) => {
@@ -57,6 +64,11 @@ function handlePageChange(newPage) {
         class="d-flex flex-column justify-content-between flex-grow-1 activity-page-table-wrap"
     >
         <!-- table -->
+        <starRatingModal
+            ref="RatingModal"
+            title="新增評論"
+            text="狼人殺"
+        ></starRatingModal>
         <table class="table align-middle table-hover mt-3">
             <thead>
                 <tr>
@@ -110,10 +122,18 @@ function handlePageChange(newPage) {
                                     前往 <span class="d-block">票券 </span>
                                 </p>
                             </button>
-                            <button
+                            <!-- <button
                                 v-if="value.status === '已使用'"
                                 type="button"
                                 class="btn btn-outline-dark btn-sm"
+                            >
+                                前往評價
+                            </button> -->
+                            <button
+                                v-if="value.status !== '已使用'"
+                                type="button"
+                                class="btn btn-outline-dark btn-sm"
+                                @click="openRatingModal"
                             >
                                 前往評價
                             </button>
---
+++ src/components/player/starRatingModal.vue
@@ -0,0 +1,172 @@
+<template>
+    <div ref="modal" class="modal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ title }}</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-grey9F">已參加的活動</p>
+                    <p class="fw-bold fs-4">{{ text }}</p>
+                    <div class="lh-lg">
+                        <p>2024-01-02 14:00 ~ 2024-03-01 14:39</p>
+                        <p>訂單編號：2134555657575</p>
+                        <p>NT$ 200</p>
+                    </div>
+
+                    <!-- 店家名稱 但ＡＰＩ沒給～～ -->
+                    <!-- <p class="fw-bold fs-4 text-center py-3">好玩工作室</p> -->
+                    <p v-if="starRatingNum" class="text-center">
+                        <span class="text-orange fw-bold">{{
+                            starRatingNum
+                        }}</span>
+                        顆星
+                    </p>
+                    <div class="d-flex justify-content-center pb-4">
+                        <div class="star-rating fs-4">
+                            <input
+                                id="sr-0-5"
+                                v-model="starRatingNum"
+                                type="radio"
+                                name="star-rating-0"
+                                value="5"
+                            />
+                            <label for="sr-0-5">★</label>
+                            <input
+                                id="sr-0-4"
+                                v-model="starRatingNum"
+                                type="radio"
+                                name="star-rating-0"
+                                value="4"
+                            />
+                            <label for="sr-0-4">★</label>
+                            <input
+                                id="sr-0-3"
+                                v-model="starRatingNum"
+                                type="radio"
+                                name="star-rating-0"
+                                value="3"
+                            />
+                            <label for="sr-0-3">★</label>
+                            <input
+                                id="sr-0-2"
+                                v-model="starRatingNum"
+                                type="radio"
+                                name="star-rating-0"
+                                value="2"
+                            />
+                            <label for="sr-0-2">★</label>
+                            <input
+                                id="sr-0-1"
+                                v-model="starRatingNum"
+                                type="radio"
+                                name="star-rating-0"
+                                value="1"
+                            />
+                            <label for="sr-0-1">★</label>
+                        </div>
+                    </div>
+                    <textarea
+                        id="comment"
+                        name="comment"
+                        placeholder="請輸入評論"
+                        class="form-control"
+                        cols="10"
+                        rows="5"
+                    ></textarea>
+                </div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        @click="myModalHide()"
+                    >
+                        取消
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        @click="myModalHide()"
+                    >
+                        送出評論
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import Modal from 'bootstrap/js/dist/modal';
+
+defineProps({
+    title: { type: String, default: '' },
+    text: { type: String, default: '' },
+});
+const modal = ref(null);
+const myModal = ref(null);
+const starRatingNum = ref(null);
+
+onMounted(() => {
+    myModal.value = new Modal(modal.value);
+});
+
+const myModalShow = () => {
+    myModal.value.show();
+};
+
+const myModalHide = () => {
+    myModal.value.hide();
+};
+
+defineExpose({
+    myModalShow,
+    myModalHide,
+});
+</script>
+<style lang="scss" scoped>
+.star-rating {
+    position: relative;
+    display: inline-flex;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+    margin: 0 -0.25rem;
+}
+
+input {
+    position: absolute;
+    opacity: 0;
+}
+
+label {
+    cursor: pointer;
+    color: grey;
+    padding: 0 0.25rem;
+    transition: color 0.15s;
+}
+
+input:checked ~ label {
+    color: #ffdd33;
+}
+
+input:hover ~ label {
+    color: goldenrod;
+    transition: none;
+}
+
+label:active {
+    color: darkgoldenrod !important;
+}
+
+input:focus-visible + label {
+    outline-offset: 1px;
+    outline: #4f46e5 solid 2px;
+}
+</style>
