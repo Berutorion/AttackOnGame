b4991e075cdc8d99cc369be91b096a29500ac103
Pei Ying <eee317@gmail.com>
2024-08-01 22:15:37 +0800

    修正問題

--- src/views/player/Form.vue
+++ src/views/player/Form.vue
@@ -5,38 +5,18 @@
                 <v-form v-slot="{ errors }">
                     <div class="mb-3">
                         <label for="playerName" class="form-label">暱稱</label>
-                        <v-field
-                            id="playerName"
-                            v-model="userForm.name"
-                            type="text"
-                            class="form-control"
-                            aria-describedby="playerName"
-                            name="暱稱"
-                            rules="required"
-                            :class="{ 'is-invalid': errors['暱稱'] }"
-                        ></v-field>
-                        <error-message
-                            name="暱稱"
-                            class="text-danger"
-                        ></error-message>
+                        <v-field id="playerName" v-model="userForm.name" type="text" class="form-control"
+                            aria-describedby="playerName" name="暱稱" rules="required"
+                            :class="{ 'is-invalid': errors['暱稱'] }"></v-field>
+                        <error-message name="暱稱" class="text-danger"></error-message>
                     </div>
                     <div class="mb-3">
                         <label for="playerPhone" class="form-label">手機</label>
-                        <v-field
-                            id="playerPhone"
-                            v-model="userForm.phone"
-                            type="text"
-                            class="form-control"
-                            aria-describedby="playerPhone"
-                            name="手機"
-                            rules="required|min:10|max:10"
-                            :class="{ 'is-invalid': errors['手機'] }"
-                        >
+                        <v-field id="playerPhone" v-model="userForm.phone" type="text" class="form-control"
+                            aria-describedby="playerPhone" name="手機" rules="required|min:10|max:10"
+                            :class="{ 'is-invalid': errors['手機'] }">
                         </v-field>
-                        <error-message
-                            name="手機"
-                            class="text-danger"
-                        ></error-message>
+                        <error-message name="手機" class="text-danger"></error-message>
                     </div>
                     <!--  
                     <div class="mb-3">
@@ -47,37 +27,22 @@
                     <div class="mb-3">
                         <h3 class="fs-6 fw-bold mt-3">喜歡的桌遊類型</h3>
                         <div class="form-check">
-                            <input
-                                id="favorite"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="角色扮演遊戲(RPG)"
-                            />
+                            <input id="favorite" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="角色扮演遊戲(RPG)" />
                             <label class="form-check-label" for="favorite">
                                 角色扮演遊戲(RPG)
                             </label>
                         </div>
                         <div class="form-check">
-                            <input
-                                id="favorite2"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="派對遊戲"
-                            />
+                            <input id="favorite2" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="派對遊戲" />
                             <label class="form-check-label" for="favorite2">
                                 派對遊戲
                             </label>
                         </div>
                         <div class="form-check">
-                            <input
-                                id="favorite3"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="策略遊戲"
-                            />
+                            <input id="favorite3" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="策略遊戲" />
                             <label class="form-check-label" for="favorite3">
                                 策略遊戲
                             </label>
@@ -95,7 +60,7 @@
 <script setup>
 import UserAccess from '@/components/UserAccess.vue';
 import PlayerAPI from '@/api/Player';
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 // import useIndexStore from '@/stores/index';
 // import ImageAPI from '@/api/Image';
